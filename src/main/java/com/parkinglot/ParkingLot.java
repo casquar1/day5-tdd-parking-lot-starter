7ff49fca9cabe8ffdc70213adbd07bf4057ef24f
Arianne <chiescake11@gmail.com>
2023-08-18 17:48:39 +0800

    feat: implement return_parking_ticket_when_park_given_parking_lot_and_car

---
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -0,0 +1,10 @@
+package com.parkinglot;
+
+public class ParkingLot {
+    private Car car;
+
+    public ParkingTicket park(Car car) {
+        this.car = car;
+        return new ParkingTicket();
+    }
+}
