57e5e1283fd96a5e3725ca6586688bfc98dbec71
Arianne <chiescake11@gmail.com>
2023-08-19 10:32:35 +0800

    refactor: rename test case

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class SmartParkingBoyTest {
     
     @Test
-    void should_park_to_first_parking_lot_when_given_a_smart_parking_boy_and_two_parking_lots_and_a_car() {
+    void should_park_to_first_parking_lot_when_park_given_a_smart_parking_boy_and_two_parking_lots_and_a_car() {
     //given
         ParkingLot firstParkingLot = new ParkingLot();
         ParkingLot secondParkingLot = new ParkingLot();
