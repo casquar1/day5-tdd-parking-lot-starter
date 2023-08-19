5863225f7900c96d6625341b5d7351e077681fd2
Arianne <chiescake11@gmail.com>
2023-08-19 10:30:48 +0800

    feat: implement park_to_first_parking_lot_when_given_a_smart_parking_boy_and_two_parking_lots_and_a_car

---
+++ src/main/java/com/parkinglot/SmartParkingBoy.java
@@ -0,0 +1,21 @@
+package com.parkinglot;
+
+import com.parkinglot.exception.NoAvailablePositionException;
+
+import java.util.List;
+
+public class SmartParkingBoy {
+    private final List<ParkingLot> parkingLots;
+
+    public SmartParkingBoy(List<ParkingLot> parkingLots) {
+        this.parkingLots = parkingLots;
+    }
+
+    public ParkingTicket park(Car car) {
+        return parkingLots.stream()
+                .filter(ParkingLot::hasAvailableCapacity)
+                .findFirst()
+                .orElseThrow(NoAvailablePositionException::new)
+                .park(car);
+    }
+}
---
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -0,0 +1,29 @@
+package com.parkinglot;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SmartParkingBoyTest {
+    
+    @Test
+    void should_park_to_first_parking_lot_when_given_a_smart_parking_boy_and_two_parking_lots_and_a_car() {
+    //given
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot secondParkingLot = new ParkingLot();
+        List<ParkingLot> parkingLots = List.of(firstParkingLot, secondParkingLot);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car car = new Car();
+     
+     //when
+        ParkingTicket parkingTicket = smartParkingBoy.park(car);
+     
+     //then
+        assertNotNull(parkingTicket);
+        assertEquals(9, firstParkingLot.getAvailableCapacity());
+        assertEquals(10, secondParkingLot.getAvailableCapacity());
+    }
+}
