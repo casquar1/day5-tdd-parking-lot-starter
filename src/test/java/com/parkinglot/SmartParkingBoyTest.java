c8bf96de53f8f83d86d0602818842656140e3835
Arianne <chiescake11@gmail.com>
2023-08-19 11:52:17 +0800

    feat: implement park_to_first_parking_lot_when_park_given_a_smart_parking_boy_and_two_parking_lots_both_having_equal_empty_positions_and_a_car

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -95,4 +95,30 @@ public class SmartParkingBoyTest {
         assertEquals(5, firstParkingLot.getAvailableCapacity());
         assertEquals(9, secondParkingLot.getAvailableCapacity());
     }
+
+    @Test
+    void should_park_to_first_parking_lot_when_park_given_a_smart_parking_boy_and_two_parking_lots_both_having_equal_empty_positions_and_a_car() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot secondParkingLot = new ParkingLot();
+        List<ParkingLot> parkingLots = List.of(firstParkingLot, secondParkingLot);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            cars.add(new Car());
+        }
+        for (Car car: cars) {
+            firstParkingLot.park(car);
+            secondParkingLot.park(car);
+        }
+        Car toParkCar = new Car();
+
+        //when
+        ParkingTicket parkingTicket = smartParkingBoy.park(toParkCar);
+
+        //then
+        assertNotNull(parkingTicket);
+        assertEquals(2, firstParkingLot.getAvailableCapacity());
+        assertEquals(3, secondParkingLot.getAvailableCapacity());
+    }
 }
