2f2fabd64a2b9ad3bf0cfea564e6402208553114
Arianne <chiescake11@gmail.com>
2023-08-19 11:34:43 +0800

    feat: implement park_to_second_parking_lot_when_park_given_a_smart_parking_boy_and_two_parking_lots_with_first_parking_lot_full_and_a_car

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartParkingBoyTest {
     
@@ -25,5 +24,24 @@ public class SmartParkingBoyTest {
         assertNotNull(parkingTicket);
         assertEquals(9, firstParkingLot.getAvailableCapacity());
         assertEquals(10, secondParkingLot.getAvailableCapacity());
+    }
+
+    @Test
+    void should_park_to_second_parking_lot_when_park_given_a_smart_parking_boy_and_two_parking_lots_with_first_parking_lot_full_and_a_car() {
+    //given
+        ParkingLot firstParkingLot = new ParkingLot(1);
+        ParkingLot secondParkingLot = new ParkingLot();
+        List<ParkingLot> parkingLots = List.of(firstParkingLot, secondParkingLot);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        firstParkingLot.park(new Car());
+        Car car = new Car();
+
+     //when
+        ParkingTicket parkingTicket = smartParkingBoy.park(car);
+
+     //then
+        assertNotNull(parkingTicket);
+        assertEquals(0, firstParkingLot.getAvailableCapacity());
+        assertEquals(9, secondParkingLot.getAvailableCapacity());
     }
 }
