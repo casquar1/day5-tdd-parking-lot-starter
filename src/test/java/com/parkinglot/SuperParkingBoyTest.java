0c6ce6b52046e0aebca1d3f31a251fe3aa93d3e9
Arianne <chiescake11@gmail.com>
2023-08-19 13:47:19 +0800

    feat: implement park_to_second_parking_lot_when_park_given_a_super_parking_boy_and_two_parking_lots_with_first_parking_lot_full_and_a_car

--- src/test/java/com/parkinglot/SuperParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperParkingBoyTest.java
@@ -26,4 +26,23 @@ public class SuperParkingBoyTest {
         assertEquals(9, firstParkingLot.getAvailableCapacity());
         assertEquals(10, secondParkingLot.getAvailableCapacity());
     }
+
+    @Test
+    void should_park_to_second_parking_lot_when_park_given_a_super_parking_boy_and_two_parking_lots_with_first_parking_lot_full_and_a_car() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot(1);
+        ParkingLot secondParkingLot = new ParkingLot();
+        List<ParkingLot> parkingLots = List.of(firstParkingLot, secondParkingLot);
+        SuperParkingBoy superParkingBoy = new SuperParkingBoy(parkingLots);
+        firstParkingLot.park(new Car());
+        Car car = new Car();
+
+        //when
+        ParkingTicket parkingTicket = superParkingBoy.park(car);
+
+        //then
+        assertNotNull(parkingTicket);
+        assertEquals(0, firstParkingLot.getAvailableCapacity());
+        assertEquals(9, secondParkingLot.getAvailableCapacity());
+    }
 }
